f394837d6b99d1f231e98bd03174a2e62a0ec423
jwill214 <williams.jesse0903@gmail.com>
2021-06-23 21:53:27 -0500

    commit-8

---
+++ src/aesthetics/Panel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Panel.css';
+
+export function Panel(props){
+    const {
+        children
+    } = props
+
+    return(
+        <div className={'profile-panel-outer'}>
+            <div className={'profile-panel-inner'}>
+                {/*{children}*/}
+                apples
+            </div>
+        </div>
+    )
+
+};
